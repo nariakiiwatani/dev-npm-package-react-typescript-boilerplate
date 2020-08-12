0bd72ce58b593c6e1f766d35b369aeda207e4a9e
nariakiiwatani <nariakiiwatani@annolab.com>
2020-08-12 09:44:33 +0900

    update several settings to avoid dependencies error between root library and demo page

--- demo/src/components/App.tsx
+++ demo/src/components/App.tsx
@@ -1,9 +1,6 @@
 
 import React, { useState } from 'react'
-// This import is only for this demo to use the newest version.
-import { ModuleName } from '../../../'
-// you would do `npm i your-package-name` and import it
-// import { ModuleName } from 'your-package-name'
+import { ModuleName } from 'your-package-name'
 
 const App = (props: {}) => {
 	return (<></>)
